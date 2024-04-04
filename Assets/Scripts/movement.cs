d02a5ebad6f4a2d05b3258b0d773226e30320e43
Benjapied <barhancet@gaming.tech>
2024-04-04 15:50:10 +0200

    f

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
+    
+    [Range (20,50)] public float _speed;
 
-    public float _speed = 10f;
     public Rigidbody2D _rb;
-    public float _jumpAmount = 10f;
+    public GameObject _feet;
+
+    [SerializeField] public float _jumpForce;
+    [SerializeField] private float _fallMultiplier;
+    [SerializeField] private float _lowJumpMultiplier = 2f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,18 +25,18 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D) == true)
-        {
-            gameObject.GetComponent<Transform>().Translate(new Vector2(10 * _speed * Time.deltaTime, 0));
-        }
-        if(Input.GetKey(KeyCode.A) == true)
-        {
-            gameObject.GetComponent<Transform>().Translate(new Vector2(-10 * _speed * Time.deltaTime, 0));
-        }
-        if(Input.GetKey(KeyCode.Escape) == true)
-        {
-            gameObject.
-        }
-    }
+         Vector2 currentVel = new Vector2(Input.GetAxis("Horizontal") * _speed * Time.deltaTime * 100, _rb.velocity.y);
 
+
+        _rb.velocity = currentVel;
+
+        if (Input.GetKeyDown(KeyCode.Space) && _feet.GetComponent<FeetPlayer>().isGrounded)
+        {
+            _rb.AddForce(new Vector2(_rb.velocity.x, _jumpForce));
+        }
+
+        //Falling
+        if ( _rb.velocity.y < 0) { _rb.velocity += Vector2.up * Physics.gravity.y * _fallMultiplier * Time.deltaTime; }
+
+    }
 }
