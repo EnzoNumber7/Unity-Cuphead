3c3a58f2aaa7cbf285928920193b21da8c08d15f
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2024-04-09 19:20:17 +0200

    Main Menu + Pause

---
+++ Assets/Enzo/Scrpits/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused;
+    [SerializeField] private GameObject pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0.0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1.0f;
+    }
+}
--- Assets/Enzo/Scrpits/PlayerMovement.cs
+++ Assets/Enzo/Scrpits/PlayerMovement.cs
@@ -45,56 +45,59 @@ public class PlayerMovementEnzo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 currentVelocity = new Vector2(0, rb.velocity.y);
+       if (PauseMenu.isPaused == false)
+        {
+            Vector2 currentVelocity = new Vector2(0, rb.velocity.y);
 
-        if (currentKunai == null)
-        {
-            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            firePointPos();
-        }
-
-        if (Input.GetKey(KeyCode.A) && blockedDirection != Direction.Left)
-        {
-            currentVelocity.x -= speed;
-        }
-        if (Input.GetKey(KeyCode.D) && blockedDirection != Direction.Right)
-        {
-            currentVelocity.x += speed;
-        }
-        if (Input.GetMouseButtonDown(0) && isUsed == false)
-        {
-            Shoot();
-            isUsed = true;
-        }
-        if (Input.GetMouseButtonDown(1) && isUsed == true)
-        {
-            if (Vector2.Distance(ropeObject.GetComponent<Rope>().topObject.transform.position, ropeObject.GetComponent<Rope>().anchor.transform.position) < 0.01)
+            if (currentKunai == null)
             {
-                ropeObject.GetComponent<Rope>().RemoveElement();
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                firePointPos();
             }
-            
-            //currentKunai.GetComponent<Kunai>().ReturnToPlayer(transform.position);
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && feet.GetComponent<Player_Feet>().isGrounded) 
-        {
-            rb.AddForce(new Vector2(0,JumpPower));
-        }
-        if(currentKunai != null)
-        {
-            if (Input.GetKeyDown(KeyCode.Q) && currentKunai.GetComponent<Kunai>().isAttached)
-            {
-                currentKunai.GetComponent<Kunai>().Detach(transform.position);
-            }
-        }
-        
 
-        if (currentKunai == null)
-        {
-            isUsed = false;
+            if (Input.GetKey(KeyCode.A) && blockedDirection != Direction.Left)
+            {
+                currentVelocity.x -= speed;
+            }
+            if (Input.GetKey(KeyCode.D) && blockedDirection != Direction.Right)
+            {
+                currentVelocity.x += speed;
+            }
+            if (Input.GetMouseButtonDown(0) && isUsed == false)
+            {
+                Shoot();
+                isUsed = true;
+            }
+            if (Input.GetMouseButtonDown(1) && isUsed == true)
+            {
+                if (Vector2.Distance(ropeObject.GetComponent<Rope>().topObject.transform.position, ropeObject.GetComponent<Rope>().anchor.transform.position) < 0.01)
+                {
+                    ropeObject.GetComponent<Rope>().RemoveElement();
+                }
+
+                //currentKunai.GetComponent<Kunai>().ReturnToPlayer(transform.position);
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && feet.GetComponent<Player_Feet>().isGrounded)
+            {
+                rb.AddForce(new Vector2(0, JumpPower));
+            }
+            if (currentKunai != null)
+            {
+                if (Input.GetKeyDown(KeyCode.Q) && currentKunai.GetComponent<Kunai>().isAttached)
+                {
+                    currentKunai.GetComponent<Kunai>().Detach(transform.position);
+                }
+            }
+
+
+            if (currentKunai == null)
+            {
+                isUsed = false;
+            }
+
+            rb.velocity = currentVelocity;
+            //CheckRange();
         }
-        
-        rb.velocity = currentVelocity;
-        //CheckRange();
     }
 
     private void Shoot()
